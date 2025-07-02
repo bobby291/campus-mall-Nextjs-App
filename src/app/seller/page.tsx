1a43f16b951c06b363275576610baca375defc4b
Bob Paschal <bobpaschal42gmail.com>
2025-07-02 08:16:47 +0100

    commit

---
+++ src/app/seller/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+    return (
+        <main className='p-6 justify-center items-center bg-gray-200'>
+            <h1 className='text-2xl font-bold'> 
+                Welcome to Seller Page under construction
+            </h1>
+        </main>
+    );
+}
---
+++ src/app/store/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+    return (
+        <main className='p-6 justify-center items-center bg-gray-200'>
+            <h1 className='text-2xl font-bold'> 
+                Welcome to Store Page under construction
+            </h1>
+        </main>
+    );
+}
